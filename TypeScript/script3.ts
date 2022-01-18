5eec94d9c2e035f26edf230e76edad22602c626e
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2022-01-19 01:10:23 +0600

    Modified

---
+++ TypeScript/script3.ts
@@ -0,0 +1,91 @@
+/**
+ * Explicit Type: set the strict type of value;
+ * 
+ * Union Type: set the mixed type of value;
+ */
+
+//Ex: explicit type with single variable
+let a: number;
+
+a = 1971;
+a = 2022;
+
+console.log(a);
+
+// Ex: explicit type with single array
+let array: number[] = [];
+
+array.push(10);
+array.push(20);
+
+console.log(array);
+
+//Ex: union type with single variable
+let b: number | string;
+
+b = 2022;
+b = "Aim to be a good software engineer";
+
+console.log(b);
+
+//Ex: union type with single array
+let arr: (number | string | boolean | object)[] = [];
+
+arr.push(40);
+arr.push("apple");
+arr.push(true);
+arr.push({name: "Nayeem"});
+
+console.log(arr);
+
+//Ex-1: explicit type with object
+let x: object;
+
+x = {
+  name: "Mr. X",
+  age: 24,
+  adult: true,
+  title: "Software Developer",
+  company: "Google",
+  phone: "01703479326",
+  email: "mrx120@gmail.com",
+};
+
+// it will work : bcz in js everything is object
+// x = [4, 5, 6];
+
+console.log(x);
+
+//Ex-2: explicit type with object
+let y: {};
+
+y = {
+  name: "Mr. X",
+  age: 24,
+  adult: true,
+  title: "Software Developer",
+  company: "Google",
+  phone: "01703479326",
+  email: "mrx120@gmail.com",
+};
+
+// it will work : bcz in js everything is object
+// y = [1, 2, 3];
+
+console.log(x);
+
+//Ex-3: explicit type with object
+let z = {
+  name: "Mr. Z",
+  age: 23,
+  adult: true,
+};
+
+//you have to follow the certain schema
+z = {
+  name: "TypeScript",
+  age: 12,
+  adult: false,
+}
+
+console.log(z);
