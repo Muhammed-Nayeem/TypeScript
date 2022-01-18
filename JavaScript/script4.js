5898c6a601e29524645bca94a0651b4d53faa574
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2022-01-19 05:11:21 +0600

    Updated

---
+++ JavaScript/script4.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * any type
+ * dynamic type
+ * these type are used in situation based
+ */
+let ab;
+ab = 50;
+ab = "Blood Group";
+console.log(ab);
+let bb = [];
+bb.push(10);
+bb.push("Bangladesh");
+console.log(bb);
+let obj;
+obj = {
+    name: "MR. X",
+    age: 24,
+};
+console.log(obj);
---
+++ TypeScript/script4.ts
@@ -0,0 +1,31 @@
+/**
+ * any type
+ * dynamic type
+ * these type are used in situation based
+ */
+
+let ab: any;
+
+ab = 50;
+ab = "Blood Group";
+
+console.log(ab);
+
+let bb:any[] = [];
+
+bb.push(10);
+bb.push("Bangladesh");
+
+console.log(bb);
+
+let obj: {
+  name: any,
+  age: any,
+};
+
+obj = {
+  name: "MR. X",
+  age: 24,
+}
+
+console.log(obj);
